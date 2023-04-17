7f2ad9c1b32cd7e738056336bca3212694eff1b5
Jobin Kurian <jobin.kurian@netcorecloud.com>
2023-04-18 00:48:48 +0530

    v3.2.0

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .target(name: "SmartPushSPM"),
         .binaryTarget(name: "SmartPush",
-                      url: "https://github.com/NetcoreSolutions/SmartPushSPM/releases/download/3.0.1/SmartPush.xcframework.zip",
-                     checksum: "b68aaa8a66f94c36bc8c8c53155d8f39639e0254be1c97dfc5f0b0e138bdc7ed")
+                      url: "https://github.com/NetcoreSolutions/SmartPushSPM/releases/download/3.2.0/SmartPush.xcframework.zip",
+                     checksum: "3404dd106e92328f5c95efd3a4f71191749892e68e80c3d7cf854a5e93e5f338")
     ]
 )
